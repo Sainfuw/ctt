47f5c89f7ec0731a218c3c7a4141db5c07064c1b
Jose Tomas <josetomascodecido@gmail.com>
2018-08-30 04:01:28 -0300

    few more vies JTCC

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
     end
   end
   root 'courses#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
