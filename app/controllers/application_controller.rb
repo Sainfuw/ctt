6b6013ea79587548a2d913ec3bbe65e283a87360
Jose Tomas <josetomascodecido@gmail.com>
2018-10-04 00:27:13 -0300

    corrections about, background colors, and back`s buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   protected
     def after_sign_in_path_for(resource)
     # return the path based on resource
+
       courses_inscriptions_path
     end
 end
